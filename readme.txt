8b1dc3eefe7aba82e0aceba149e6ccb62f059222
Hossein <hossein.seyedmehdi@gmail.com>
2017-07-02 12:12:54 -0400

    Upload with Docker + flask web esrver works

--- readme.txt
+++ readme.txt
@@ -1,6 +1,10 @@
 on EC2 instance:
 sudo service docker start
 
+
+ssh -i "aws-north-virginia-seyedmehdi-key-pair.pem" ubuntu@ec2-52-91-171-139.compute-1.amazonaws.com
+ 
 http://www.ybrikman.com/writing/2015/11/11/running-docker-aws-ground-up/
 
+docker build -t temdy/puppyfaceimage ./web_engine/
 docker push temdy/puppyfaceimage
--- web_engine/route.py
+++ web_engine/route.py
@@ -1,19 +1,33 @@
-from web_engine import app
+#from web_engine import app
 
 # for running on the local machine: 
-if __name__ == '__main__': 
-    from flask import Flask
-    app = Flask('web_engine') #app = Flask('web_engine')
+#if __name__ == '__main__': 
+from flask import Flask
+app = Flask('web_engine') #app = Flask('web_engine')
 
+import os
 from time import localtime, strftime
 from redis import Redis 
-from flask import session, request, redirect
-from flask.templating import render_template
-from .config import BaseConfig
+#from .config import BaseConfig
+class BaseConfig(object):
+    SECRET_KEY = 'none'
+    DEBUG = False
+    REDIS_HOST = 'redis'
+    REDIS_PORT = 6379
+
+
+from flask import render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+#UPLOAD_FOLDER = 'web_engine/uploads/'
+UPLOAD_FOLDER = 'uploads/'
 
 app.config.from_object(BaseConfig)
 
-redis = Redis(host = app.config['REDIS_HOST'], port = app.config['REDIS_PORT'])
+#redis = Redis(host = app.config['REDIS_HOST'], port = app.config['REDIS_PORT'])
+redis = None
+
 
 def site_analytics():
     '''
@@ -27,20 +41,68 @@ def site_analytics():
         redis.set('first_visit_day', strftime("%d %b %Y", localtime()))    
     redis.incr('hits')
 
-@app.route('/')
+@app.route('/home')
 def home_page():
     site_analytics()
     
-    searchword = request.args.get('q', '')
-    results = ''
-    if searchword.lower() == 'test' or searchword.lower() == 't': 
-        pass 
-
     return render_template('first_page.html', 
-                           search_results = results, 
-                           search_expr = searchword,
                            visit_number = redis.get('hits').decode(),
                            since = redis.get('first_visit_day').decode())
+
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        
+        #return "success"
+        import subprocess
+        subprocess.call(['chmod', '0744', file_path])
+        
+        from os import listdir
+        from os.path import isfile, join
+        onlyfiles = [f for f in listdir(UPLOAD_FOLDER) if isfile(join(UPLOAD_FOLDER, f))]
+        #return str(onlyfiles)
+    
+        return redirect(url_for('uploaded_file', filename=filename))
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER,
+                               filename)
 
 
 @app.route('/test')
@@ -57,6 +119,5 @@ def test():
 
     return output 
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
